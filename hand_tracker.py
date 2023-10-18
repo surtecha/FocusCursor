78f420497ac63ffab3ce0c2c90fa682c80e94fbc
Suryateja Challa <challasuryateja29@gmail.com>
2023-10-18 18:38:49 +0530

    Updated hand_tracker.py using dedicated hands library instead of holistic.

--- hand_tracker.py
+++ hand_tracker.py
@@ -1,34 +1,27 @@
 import cv2
 import mediapipe as mp
 
-mp_draw = mp.solutions.drawing_utils
-mp_holistic = mp.solutions.holistic
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
 
 cap = cv2.VideoCapture(0)
 
-with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-
-    while True:
+with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
+    while cap.isOpened():
         ret, frame = cap.read()
         frame = cv2.flip(frame, 1)
 
-        img_h, img_w = frame.shape[:2]
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+        results = hands.process(frame)
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-        results = holistic.process(frame)
+        if results.multi_hand_landmarks:
+            for num, hand in enumerate(results.multi_hand_landmarks):
+                mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS)
 
-        #Right hand landmarks
-        mp_draw.draw_landmarks(frame, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
-                               mp_draw.DrawingSpec(color=(240, 15, 0), thickness=2, circle_radius=4),
-                               mp_draw.DrawingSpec(color=(255, 255, 255), thickness=2, circle_radius=2)
-                              )
-
-        #Left hand landmarks
-        mp_draw.draw_landmarks(frame, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
-                               mp_draw.DrawingSpec(color=(15, 255, 0), thickness=2, circle_radius=4),
-                               mp_draw.DrawingSpec(color=(255, 255, 255), thickness=2, circle_radius=2)
-                               )
-
-        cv2.imshow('Hand Tracking', frame)
+        cv2.imshow("Hand Tracking", image)
 
         key = cv2.waitKey(1)
         if key == ord('q'):
