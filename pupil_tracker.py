719b1b23b3ca3532e33ce232bc71edf6400150b7
Suryateja Challa <challasuryateja29@gmail.com>
2023-10-18 18:39:42 +0530

    Updated pupil_tracker.py using BGR2RGB conversions.

--- pupil_tracker.py
+++ pupil_tracker.py
@@ -20,12 +20,16 @@ with mp_face_mesh.FaceMesh(
 ) as face_mesh:
 
     while True:
-        _, frame = cap.read()
+        ret, frame = cap.read()
         frame = cv2.flip(frame, 1)
 
-        img_h, img_w = frame.shape[:2]
-
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
         results = face_mesh.process(frame)
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        img_h, img_w = image.shape[:2]
 
         if results.multi_face_landmarks:
             mesh_points = np.array([np.multiply([p.x, p.y], [img_w, img_h]).astype(int) for p in results.multi_face_landmarks[0].landmark])
@@ -39,30 +43,30 @@ with mp_face_mesh.FaceMesh(
             # cv2.polylines(frame, [mesh_points[RIGHT_IRIS]], True, (255, 0, 0), 1, cv2.LINE_AA)
 
             # Drawing circle around tracked iris
-            (l_cx, lcy), l_radius = cv2.minEnclosingCircle(mesh_points[LEFT_IRIS])
-            (r_cx, r_cy), r_radius = cv2.minEnclosingCircle(mesh_points[RIGHT_IRIS])
-            center_left = np.array([l_cx, lcy], dtype=np.int32)
-            center_right = np.array([r_cx, r_cy], dtype=np.int32)
-            cv2.circle(frame, center_left, int(l_radius), (0, 255, 0), 1, cv2.LINE_AA)
-            cv2.circle(frame, center_right, int(r_radius), (0, 255, 0), 1, cv2.LINE_AA)
+            # (l_cx, lcy), l_radius = cv2.minEnclosingCircle(mesh_points[LEFT_IRIS])
+            # (r_cx, r_cy), r_radius = cv2.minEnclosingCircle(mesh_points[RIGHT_IRIS])
+            # center_left = np.array([l_cx, lcy], dtype=np.int32)
+            # center_right = np.array([r_cx, r_cy], dtype=np.int32)
+            # cv2.circle(frame, center_left, int(l_radius), (0, 255, 0), 1, cv2.LINE_AA)
+            # cv2.circle(frame, center_right, int(r_radius), (0, 255, 0), 1, cv2.LINE_AA)
 
             # Storing location of pupils
             left_pupil_x, left_pupil_y = mesh_points[468]
             right_pupil_x, right_pupil_y = mesh_points[473]
 
             # Marking the pupils
-            cv2.circle(frame, mesh_points[468], 2, (0, 0, 255), 1, cv2.LINE_4)
-            cv2.circle(frame, mesh_points[473], 2, (0, 0, 255), 1, cv2.LINE_4)
+            cv2.circle(image, mesh_points[468], 2, (0, 0, 255), 2, cv2.LINE_4)
+            cv2.circle(image, mesh_points[473], 2, (0, 0, 255), 2, cv2.LINE_4)
 
             # Centroid of position of pupil
             x = (left_pupil_x + right_pupil_x) // 2
             y = (left_pupil_y + right_pupil_y) // 2
-            cv2.circle(frame, (x, y), 2, (255, 0, 0), 1, cv2.LINE_4)
+            cv2.circle(image, (x, y), 2, (0, 255, 0), 2, cv2.LINE_4)
 
             # Position of centroid
             print("x={}, y={}".format(x, y))
 
-        cv2.imshow('Pupil Tracking', frame)
+        cv2.imshow('Pupil Tracking', image)
 
         key = cv2.waitKey(1)
         if key == ord('q'):
